8e613659472d883b7f211eef38ea9a77ae388d00
Adrian Fjellberg <adrian@adapted.no>
2026-02-05 12:24:28 +0100

    WIP

--- app/app.vue
+++ app/app.vue
@@ -5,19 +5,19 @@
 <script setup>
 
 useSeoMeta({
-  title: 'Ølympiske Leker 2026',
-  description: 'Velkommen til Ølympiske Leker 2026! Én gang i året leggir fornuften igjen hjemme, og en gjeng på 11 godt voksne mannfolk setter kursen mot Nesbyen for det som har blitt en høyt elsket tradisjon: hyttetur med Ølympiske Leker.',
-  twitterTitle: 'Ølympiske Leker 2026',
-  twitterDescription: 'Velkommen til Ølympiske Leker 2026! Én gang i året leggir fornuften igjen hjemme, og en gjeng på 11 godt voksne mannfolk setter kursen mot Nesbyen for det som har blitt en høyt elsket tradisjon: hyttetur med Ølympiske Leker.',
+  title: 'Ølympiske Leker 2025',
+  description: 'Velkommen til Ølympiske Leker 2025! Én gang i året leggir fornuften igjen hjemme, og en gjeng på 11 godt voksne mannfolk setter kursen mot Nesbyen for det som har blitt en høyt elsket tradisjon: hyttetur med Ølympiske Leker.',
+  twitterTitle: 'Ølympiske Leker 2025',
+  twitterDescription: 'Velkommen til Ølympiske Leker 2025! Én gang i året leggir fornuften igjen hjemme, og en gjeng på 11 godt voksne mannfolk setter kursen mot Nesbyen for det som har blitt en høyt elsket tradisjon: hyttetur med Ølympiske Leker.',
   twitterImage: 'https://olympiske-leker-2025.pages.dev/olympiskeleker_og_card.png',
   twitterCard: 'summary_large_image',
-  ogTitle: 'Ølympiske Leker 2026',
-  ogDescription: 'Velkommen til Ølympiske Leker 2026! Én gang i året leggir fornuften igjen hjemme, og en gjeng på 11 godt voksne mannfolk setter kursen mot Nesbyen for det som har blitt en høyt elsket tradisjon: hyttetur med Ølympiske Leker.',
+  ogTitle: 'Ølympiske Leker 2025',
+  ogDescription: 'Velkommen til Ølympiske Leker 2025! Én gang i året leggir fornuften igjen hjemme, og en gjeng på 11 godt voksne mannfolk setter kursen mot Nesbyen for det som har blitt en høyt elsket tradisjon: hyttetur med Ølympiske Leker.',
   ogImage: 'https://olympiske-leker-2025.pages.dev/olympiskeleker_og_card.png',
 });
 
 useHead({
-  titleTemplate: '%s - Ølympiske Leker 2026',
+  titleTemplate: '%s - Ølympiske Leker 2025',
   htmlAttrs: {
     lang: 'en',
   },
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,8 +4,8 @@
     <div class="border-b-2 border-gray-100 h-[70px] flex items-center justify-center">
       <router-link to="/">
         <img
-          src="/img/olympiske_leker_2026_logo_horizontal.svg"
-          alt="Ølympiske Leker 2026 Text Logo"
+          src="/img/olympiske_leker_2025_logo_horizontal.svg"
+          alt="Ølympiske Leker 2025 Text Logo"
           class="h-[30px]"
         >
       </router-link>
@@ -13,8 +13,8 @@
 
     <!-- Hero Section -->
     <div class="flex flex-col items-center justify-center h-[calc(100vh-200px)] max-h-[400px] text-center px-4">
-      <img src="/img/olympiske_leker_2026_logo_horizontal.svg" alt="Olympiske Leker 2026 Logo" class="w-32 mb-4">
-      <h1 class="text-4xl font-bold mb-4">De Ølympiske<br>Leker 2026</h1>
+      <img src="/img/olympiske_leker_2025_logo_horizontal.svg" alt="Olympiske Leker 2025 Logo" class="w-32 mb-4">
+      <h1 class="text-4xl font-bold mb-4">De Ølympiske<br>Leker 2025</h1>
     </div>
 
     <!-- Countdown Section -->
