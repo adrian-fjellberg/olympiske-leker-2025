fe2d14137e641e8a23cdf4d5c7c9d7489fdd5c4a
Adrian Fjellberg <adrian@adapted.no>
2026-02-05 12:07:00 +0100

    Added favicons.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,5 +29,19 @@ export default defineNuxtConfig({
     '@nuxt/eslint',
     '@nuxt/fonts',
     '@nuxt/icon'
-  ]
+  ],
+  app: {
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/favicon-96x96.png' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'manifest', href: '/site.webmanifest' },
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+      ],
+      meta: [
+        { name: 'theme-color', content: '#ffffff' },
+      ],
+    },
+  }
 })
